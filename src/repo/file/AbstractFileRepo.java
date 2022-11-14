fa62bd6a05f0df758af78fbda14a7962ee98ecb1
Andrei <andrei.iacob@stud.ubbcluj.ro>
2022-11-14 16:52:49 +0200

    Lab 5 MAP Social Network

--- src/repo/file/AbstractFileRepo.java
+++ src/repo/file/AbstractFileRepo.java
@@ -19,14 +19,14 @@ public abstract class AbstractFileRepo<T> extends UserMemoryRepository {
     }
 
     private void loadData() {
-        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 List<String> attrs = Arrays.asList(line.split(";"));
                 User entity = extractEntity(attrs);
                 save(entity);
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -34,27 +34,39 @@ public abstract class AbstractFileRepo<T> extends UserMemoryRepository {
     @Override
     public User save(User entity) {
         User e = super.save(entity);
-        if(e == null){
+        if (e == null) {
             writeToFile(entity);
         }
         return e;
     }
 
     @Override
-    public void delete(int ID){
-       super.delete(ID);
+    public void delete(int ID) {
+        super.delete(ID);
+        for(User u: getAll()){
+            System.out.println(u);
+        }
+        for(User u: getAll()){
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName, false))) {
+                bw.write(createEntityAsString(u));
+                bw.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void writeToFile(User entity) {
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(fileName, true))){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName, true))) {
             bw.write(createEntityAsString(entity));
             bw.newLine();
-        } catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public abstract User extractEntity(List<String> attrs);
+
     protected abstract String createEntityAsString(User entity);
 
 }
